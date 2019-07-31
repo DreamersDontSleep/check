736e0f3ce2f2f7f43b4c3bf0ddb5112875d4dd4c
有梦想的人不睡觉 <690766140@qq.com>
2019-08-01 00:19:51 +0800

    提交代码

--- src/views/entryList/index.vue
+++ src/views/entryList/index.vue
@@ -98,7 +98,7 @@
             </el-table-column>
 						<el-table-column label="盖章结果">
 						  <template slot-scope="scope">
-							  <div v-if="scope.row.stampState == '' || scope.row.stampState == null">
+							  <div v-if="scope.row.stampState == '' || scope.row.stampState == null || scope.row.stampState == 0">
 							  	<span style="color: rgba(107, 107, 107, 0.647058823529412);">未盖章</span>
 							  </div>
 							  <div v-if="scope.row.stampState == 1">
